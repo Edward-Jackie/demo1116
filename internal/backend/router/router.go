ff962b2ed456ae2bdf40ed3d5222291a8a1690cb
Ed-j <419130032@qq.com>
2025-01-15 18:44:29 +0800

    feat

--- internal/backend/router/router.go
+++ internal/backend/router/router.go
@@ -1,15 +1,24 @@
 package backend
 
 import (
+	"demo1116/middleware"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func InitRouter(r *gin.RouterGroup) {
-	b := r.Group("/")
+	b := r.Group("/internal")
+	// 添加跨域
+	b.Use(middleware.Cors())
 	{
 		b.GET("/", func(c *gin.Context) {
 			c.JSON(http.StatusOK, "Hello User")
+		})
+
+		b.DELETE("/user/:id", func(c *gin.Context) {
+			p := c.Param("id")
+			fmt.Println(p)
 		})
 	}
 }
--- middleware/cor.go
+++ middleware/cor.go
@@ -1,3 +1,27 @@
 package middleware
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 // 跨越中间件
+func Cors() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// 请求来源-所有
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		// 允许请求
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		// 允许头文件
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		if c.Request.Method == http.MethodOptions {
+			c.AbortWithStatus(http.StatusNoContent)
+			return
+		}
+
+		c.Next()
+	}
+}
